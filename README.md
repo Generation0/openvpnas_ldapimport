64e05d86371813b06519b493fdffd73ce70c8bf5
Generation0 <t.loeb@ostfalia.de>
2015-09-15 14:37:00 +0200

    CHANGED more Pathvariables, Install instructions

--- README.md
+++ README.md
@@ -3,14 +3,13 @@ Import Groups from LDAP into your OpenVPN Access Server
 
 # Quicksetup
 
-**At first you need to change the Path to your Group in ldap_import.py**
+**At first you need to change the Path to your Group in ldap_import.py and check the Path to the Openvpn Access Server Script Folder**
 
 Then copy the script into the script folder of openvpnas and launch the script in a virtual environment
 ```
-cp ldap_import.py /usr/local/openvpn_as/scripts
 cd py3env
 source bin/activate
-cd /usr/local/openvpn_as/scripts
+cd ..
 python ldap_import
 ```
 
--- ldap_import.py
+++ ldap_import.py
@@ -7,8 +7,12 @@ from ldap3 import Server, Connection, SIMPLE, SYNC, ASYNC, SUBTREE, ALL
 
 uri = input("LDAP Connection: ") 							
 user = input("Username: ")	 	 							
-password = getpass(prompt="Password: ", stream=None)		
+password = getpass(prompt="Password: ", stream=None)	
+
+### IMPORTANT Change this Lines to set your Path to the Group Folder	
 ldap_group_path="OU=Users,OU=Company,DC=company ,DC=local"
+### Path to the Scripts Folder where you can find sacli
+o_path = "/usr/local/openvpn_as/scripts"
 
 group = input("Enter Group [All Users]:  ")
 import_group = "OU="+group+","
@@ -23,11 +27,11 @@ c.search(ldap_path,'(&(objectclass=user)(objectcategory=person))', SUBTREE, attr
 response = c.response_to_json()
 result = c.result
 parsed_json = json.loads(response)
-print("importing", end=" ")
+print("importing"),
 for entry in parsed_json['entries']:
 	ma_name = entry['attributes']['sAMAccountName']
-	print(".", end=" ")
-	os.system("./sacli --user %s --key conn_group --value '%s' UserPropPut" %(ma_name, group))
+	print("."),
+	os.system(o_path+"/sacli --user %s --key conn_group --value '%s' UserPropPut" %(ma_name, group))
 print("\nimport finished")
 
 
